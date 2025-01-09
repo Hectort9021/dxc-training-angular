c0166c15b98e3c9a9f7c58c5f1d08a7e90feed9f
Hector Tabarez <hector.tabarez-de-tolentino-camacho@dxc.com>
2025-01-09 14:22:19 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent,
+            MenuComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'store';
+}
